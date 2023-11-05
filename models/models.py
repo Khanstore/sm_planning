6b537aa37f67d1f79635e42ac767af6faca5f63a
Khanstore <dmashraf@gmail.com>
2023-11-05 20:30:49 +0600

    total time doesnt display

--- models/models.py
+++ models/models.py
@@ -2,6 +2,7 @@
 # Part of eagle. See LICENSE file for full copyright and licensing details.
 
 from odoo import fields, models, api
+from collections import defaultdict
 from datetime import date, datetime, time
 import calendar
 from dateutil import relativedelta
@@ -18,3 +19,17 @@ class PlanningRole(models.Model):
 class planningSlot(models.Model):
     _inherit = "planning.slot"
     site_id=fields.Many2one('res.partner',string="Site")
+
+
+    def _group_slots_by_role(self):
+        grouped_slots = defaultdict(self.browse)
+        for slot in self.sorted(key=lambda s: s.role_id.name or ''):
+            grouped_slots[slot.role_id] |= slot
+        return grouped_slots
+    def totalAllocatedHours(self):
+        total=0
+        for rec in self:
+            total=total+rec.allocated_hours
+        return total
+
+
